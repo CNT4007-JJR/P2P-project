2b3ccde1f093a4b94d16a92836577b54ac806dc5
Joseph Maisevich <joseph.maisevich@ufl.edu>
2020-11-02 16:52:26 -0500

    Message handler uses message creation

--- src/edu/ufl/jjr/peer/MessageHandler.java
+++ src/edu/ufl/jjr/peer/MessageHandler.java
@@ -1,33 +1,60 @@
 package edu.ufl.jjr.peer;
 
+import edu.ufl.jjr.MessageCreator.MessageCreator;
+
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 
 public class MessageHandler implements Runnable {
     private Peer peer;
+    private int remotePeerId;
     private ObjectOutputStream out;
     private ObjectInputStream in;
     private Socket socket;
+    private MessageCreator creator;
 
     public MessageHandler(ObjectInputStream in, ObjectOutputStream out, Peer peer, Socket socket){
         this.in = in;
         this.out = out;
         this.peer = peer;
         this.socket = socket;
+        creator = new MessageCreator();
     }
 
     public void run(){
-        String handshake = "handshake message";
+        //String handshake = "handshake message";
+        byte[] handshake = new byte[0];
+        try {
+            handshake = creator.handshakeMessage(peer.peerID);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         //send handshake message
         send(handshake);
         while(true){
             try {
-                String message = (String)in.readObject();
-                if(message.equals(handshake)) {
-                    System.out.println(peer.peerID + " received this message " + message);
-                    send("message received");
+                byte [] message = (byte[])in.readObject();
+
+                if(message[4] == 5){
+                    System.out.println("recieved bitfield message" + remotePeerId);
+                }else{
+                ByteBuffer buffer =  ByteBuffer.wrap(message);
+                byte[] header = new byte[18];
+                buffer.get(header,0,18);
+                String handShakeString = new String(header, StandardCharsets.UTF_8);
+
+                if(handShakeString.equals("P2PFILESHARINGPROJ")) {
+                    byte[] peerId = new byte[4];
+                    System.arraycopy(message, 28, peerId, 0, 4);
+                    int peerIdInt = ByteBuffer.wrap(peerId).getInt();
+                    System.out.println(peer.peerID + " received the handshake message from " + peerIdInt);
+                    remotePeerId = peerIdInt;
+                    send(creator.bitFieldMessage(peer.bitfield));
+                    }
                 }
 
             } catch (IOException e) {
@@ -38,7 +65,7 @@ public class MessageHandler implements Runnable {
         }
     }
 
-    public void send(String msg){ //will need to be changed to byte [] msg
+    public void send(byte [] msg){ //will need to be changed to byte [] msg
         try{
             out.writeObject(msg);
             out.flush();
